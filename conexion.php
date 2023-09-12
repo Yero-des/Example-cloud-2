ab8450c5b682535e1b4d0ca701dd45fb47562cb5
Yero-des <yero_zc@hotmail.com>
2023-09-11 21:08:42 -0500

    First production db :)

--- conexion.php
+++ conexion.php
@@ -2,11 +2,11 @@
 
 	function conexion(){
 
-	$host = "host=xxxx";
-	$port = "port=xxxx";
-	$dbname = "dbname=xxxx";
-	$user = "user=xxxx";
-	$password = "password=xxxx";
+	$host = "host=containers-us-west-39.railway.app";
+	$port = "port=7812";
+	$dbname = "dbname=railway";
+	$user = "user=postgres";
+	$password = "password=B6druld1rAJslBiCufp2";
 
 	$db = pg_connect("$host $port $dbname $user $password");
 
